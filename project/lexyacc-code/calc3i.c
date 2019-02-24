8e4d14f7d19204b938f160ac2020e507e8527bf9
BeniTheDuke <benjamin@ibu.ch>
2019-02-24 18:40:49 +0100

    changes from kush + gitignore

--- project/lexyacc-code/calc3i.c
+++ project/lexyacc-code/calc3i.c
@@ -20,7 +20,7 @@ int ex(nodeType *p) {
         case WHILE:
             printf("L%03d:\n", lbl1 = lbl++);
             ex(p->opr.op[0]);
-            printf("\tjz\tL%03d\n", lbl2 = lbl++);
+            // printf("\tjz\tL%03d\n", lbl2 = lbl++);
             ex(p->opr.op[1]);
             printf("\tjmp\tL%03d\n", lbl1);
             printf("L%03d:\n", lbl2);
@@ -29,7 +29,7 @@ int ex(nodeType *p) {
             ex(p->opr.op[0]);
             if (p->opr.nops > 2) {
                 /* if else */
-                printf("\tjz\tL%03d\n", lbl1 = lbl++);
+                // printf("\tjz\tL%03d\n", lbl1 = lbl++);
                 ex(p->opr.op[1]);
                 printf("\tjmp\tL%03d\n", lbl2 = lbl++);
                 printf("L%03d:\n", lbl1);
@@ -37,7 +37,7 @@ int ex(nodeType *p) {
                 printf("L%03d:\n", lbl2);
             } else {
                 /* if */
-                printf("\tjz\tL%03d\n", lbl1 = lbl++);
+                // printf("\tjz\tL%03d\n", lbl1 = lbl++);
                 ex(p->opr.op[1]);
                 printf("L%03d:\n", lbl1);
             }
@@ -45,6 +45,11 @@ int ex(nodeType *p) {
         case PRINT:     
             ex(p->opr.op[0]);
             printf("\tprint\n");
+		//mov	edx,4		; message length
+		//mov	ecx,msg		; message to write
+		//mov	ebx,1		; file descriptor (stdout)
+		//mov	eax,4		; system call number (sys_write)
+		//int	0x80		; call kernel
             break;
         case '=':       
             ex(p->opr.op[1]);
@@ -54,20 +59,29 @@ int ex(nodeType *p) {
             ex(p->opr.op[0]);
             printf("\tneg\n");
             break;
+	case FACT:
+  	    ex(p->opr.op[0]);
+	    printf("\tfact\n");
+	    break;
+	case LNTWO:
+	    ex(p->opr.op[0]);
+	    printf("\tlntwo\n");
+	    break;
         default:
             ex(p->opr.op[0]);
             ex(p->opr.op[1]);
             switch(p->opr.oper) {
-            case '+':   printf("\tadd\n"); break;
-            case '-':   printf("\tsub\n"); break; 
-            case '*':   printf("\tmul\n"); break;
-            case '/':   printf("\tdiv\n"); break;
-            case '<':   printf("\tcompLT\n"); break;
-            case '>':   printf("\tcompGT\n"); break;
-            case GE:    printf("\tcompGE\n"); break;
-            case LE:    printf("\tcompLE\n"); break;
-            case NE:    printf("\tcompNE\n"); break;
-            case EQ:    printf("\tcompEQ\n"); break;
+	    case GCD:   printf("\tpop\trdx\n"); printf("\tpop\trsx\n"); printf("\tpop\trsx\n"); printf("\tcall gcd\n"); break;
+            case '+':   printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tadd\trax,rdx\n"); break;
+            case '-':   printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tsub\trax,rdx\n"); break;
+            case '*':   printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tmul\trdx\n"); break;
+            case '/':   printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tmov\tRBX,0\n"); printf("\tdiv\trdx,0\n"); break;
+            case '<':   printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tcmp\trax,rdx\n"); printf("\tjl\tL%03d\n", lbl1 = lbl++); break;
+            case '>':   printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tcmp\trax,rdx\n"); printf("\tjg\tL%03d\n", lbl1 = lbl++); break; 
+            case GE:    printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tcmp\trax,rdx\n"); printf("\tjge\tL%03d\n", lbl2 = lbl++); break;
+            case LE:    printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tcmp\trax,rdx\n"); printf("\tjle\tL%03d\n", lbl2 = lbl++); break;
+            case NE:    printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tcmp\trax,rdx\n"); printf("\tjne\tL%03d\n", lbl2 = lbl++); break;
+            case EQ:    printf("\tpop\trax\n"); printf("\tpop\trdx\n"); printf("\tcmp\trax,rdx\n"); printf("\tje\tL%03d\n", lbl2 = lbl++); break;
             }
         }
     }
